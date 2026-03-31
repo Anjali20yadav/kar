c1a11d2a5155b4cc2b4d5518cb921ce3a07741bd
Yogesh Rajput <141839046+YogeshRajput82@users.noreply.github.com>
2026-04-01 00:21:18 +0530

    fixed cors issue

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://kar-black.vercel.app"
+    ],
     credentials: true
 }))
 
